03509a3b73d9a87121861d2df04f84489db497b3
TigranBalanyan <tbalanyan@gmail.com>
2018-10-19 15:36:43 +0400

    Action Call API code.

---
+++ ActionCallCRM/ActionCallCRM/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesInfoCall
+{
+    class SalesInfoCall
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Username: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Password: ");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Enter your organization URL : ");
+            string URL = Console.ReadLine();
+
+
+            var credentials = new ClientCredentials();
+            credentials.UserName.UserName = userName;
+            credentials.UserName.Password = password;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Uri OrganizationUri = new Uri(URL);
+
+            try
+            {
+                using (OrganizationServiceProxy serviceProxy = new OrganizationServiceProxy(OrganizationUri, null, credentials, null))
+                {
+                    IOrganizationService service = (IOrganizationService)serviceProxy;
+
+                    OrganizationRequest request = new OrganizationRequest("new_sales_info_retrieval");
+
+                    OrganizationResponse response = service.Execute(request);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
+
+
+
+            
