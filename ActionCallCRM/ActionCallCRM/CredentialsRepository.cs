fcf6a8a780e74b630ea177cad962455ab6548d59
TigranBalanyan <tbalanyan@gmail.com>
2018-10-22 20:41:21 +0400

    Added CredentialsRepository, and added additional functionality.

---
+++ ActionCallCRM/ActionCallCRM/CredentialsRepository.cs
@@ -0,0 +1,74 @@
+﻿using CredentialManagement;
+
+namespace SalesInfoCall
+{
+    internal class CredentialsRepository
+    {
+
+        private const string PasswordName = "ServerPassword";
+        private const string Username = "ServerUsername";
+
+        public void SavePassword(string password)
+        {
+            using (var cred = new Credential())
+            {
+                cred.Password = password;
+                cred.Target = PasswordName;
+                cred.Type = CredentialType.Generic;
+                cred.PersistanceType = PersistanceType.LocalComputer;
+                cred.Save();
+            }
+        }
+
+        public string GetPassword()
+        {
+            using (var cred = new Credential())
+            {
+                cred.Target = PasswordName;
+                cred.Load();
+                return cred.Password;
+            }
+        }
+
+        public void ClearPassword()
+        {
+            using (var cred = new Credential())
+            {
+                cred.Target = PasswordName;
+                cred.Delete();
+            }
+        }
+
+
+        public void SaveUsername(string username)
+        {
+            using (var cred = new Credential())
+            {
+                cred.Username = username;
+                cred.Target = Username;
+                cred.Type = CredentialType.Generic;
+                cred.PersistanceType = PersistanceType.LocalComputer;
+                cred.Save();
+            }
+        }
+
+        public string GetUsername()
+        {
+            using (var cred = new Credential())
+            {
+                cred.Target = Username;
+                cred.Load();
+                return cred.Username;
+            }
+        }
+
+        public void ClearUsername()
+        {
+            using (var cred = new Credential())
+            {
+                cred.Target = Username;
+                cred.Delete();
+            }
+        }
+    }
+}
--- ActionCallCRM/ActionCallCRM/Program.cs
+++ ActionCallCRM/ActionCallCRM/Program.cs
@@ -2,54 +2,89 @@
 using Microsoft.Xrm.Sdk.Client;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SalesInfoCall
+namespace SalesInfoCall 
 {
-    class SalesInfoCall
+    partial class SalesInfoCall
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your Username: ");
-            string userName = Console.ReadLine();
-
-            Console.WriteLine("Enter your Password: ");
-            string password = Console.ReadLine();
-
-            Console.WriteLine("Enter your organization URL : ");
-            string URL = Console.ReadLine();
-
-
-            var credentials = new ClientCredentials();
-            credentials.UserName.UserName = userName;
-            credentials.UserName.Password = password;
-
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            Uri OrganizationUri = new Uri(URL);
-
-            try
+            while (true)
             {
-                using (OrganizationServiceProxy serviceProxy = new OrganizationServiceProxy(OrganizationUri, null, credentials, null))
+               
+                CredentialsRepository userData = new CredentialsRepository();
+                if (userData.GetUsername() == string.Empty || userData.GetPassword() == string.Empty)
                 {
-                    IOrganizationService service = serviceProxy;
+                    Console.WriteLine("Enter your Username: ");
+                    userData.SaveUsername(Console.ReadLine());
 
-                    OrganizationRequest request = new OrganizationRequest("****");
-
-                    OrganizationResponse response = service.Execute(request);
+                    Console.WriteLine("Enter your Password: ");
+                    userData.SavePassword(Console.ReadLine());
                 }
-            }
-            catch(Exception ex)
-            {
-                throw new Exception(ex.Message);
+
+           //     Console.WriteLine(userData.GetUsername());
+            //    Console.WriteLine(userData.GetPassword());
+
+                string URL = string.Empty;
+                string path = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location).FullName;
+                string fileName = Path.Combine(path, "text.txt");
+
+                if (File.Exists(fileName) && File.ReadAllText(fileName).Length != 0)
+                {
+                    URL = File.ReadAllText(fileName);
+                }
+                else
+                {
+                    File.WriteAllText(fileName, string.Empty);
+                    Console.WriteLine("Enter your organization URL : ");
+                    URL = Console.ReadLine();
+                    File.WriteAllText(fileName, URL);
+                }
+
+              //  Console.WriteLine(URL);
+
+
+                var credentials = new ClientCredentials();
+                credentials.UserName.UserName = userData.GetUsername();
+                credentials.UserName.Password = userData.GetPassword();
+
+                Console.WriteLine(URL);
+                Console.WriteLine(credentials.UserName.UserName);
+                Console.WriteLine(credentials.UserName.Password);
+                try
+                {
+                  //  ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                    Uri OrganizationUri = new Uri(URL);
+
+                    using (OrganizationServiceProxy serviceProxy = new OrganizationServiceProxy(OrganizationUri, null, credentials, null))
+                    {
+                        IOrganizationService service = (IOrganizationService)serviceProxy;
+                        Console.WriteLine("ancav!");
+                        OrganizationRequest request = new OrganizationRequest("new_sales_info_retrieval");
+
+                        OrganizationResponse response = service.Execute(request);
+                    }
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Please Check connection, or try again!");
+                    userData.ClearPassword();
+                    userData.ClearUsername();
+                    File.WriteAllText(fileName, string.Empty);
+                    continue;
+                }
+
+                break;
             }
         }
     }
 }
-
-
 
             
